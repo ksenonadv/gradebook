a410b21d764238cd9d216c3892f4d6e85df23be1
ksenonadv <cristeastefan1999@gmail.com>
2025-03-27 12:23:02 +0200

    GBK-23 <Fix title test>

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'grade-book' title`, () => {
+  it(`should have the 'Grade Book' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('grade-book');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, grade-book');
+    expect(app.title).toEqual('Grade Book');
   });
 });
