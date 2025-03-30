5d96521edd252a9a09db2598c7b1af400c8a4e9f
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2025-03-30 20:45:50 +0300

    GBK-7 <Add design for profile>

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -3,11 +3,21 @@ import { RouterModule } from '@angular/router';
 import { User } from '../../interfaces/user.interface';
 import { AuthService } from '../../services/auth.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationsService } from '../../services/notifications.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    RouterModule
+    RouterModule,
+    FileUploadModule,
+    ButtonModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
@@ -17,11 +27,29 @@ export class ProfileComponent {
   private readonly authService = inject(AuthService); 
   public user: User | undefined = undefined;
 
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly notificationsService = inject(NotificationsService);
+
+  public form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+
   private destroyRef = inject(DestroyRef);
 
   constructor() {
     this.authService.userData$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((user: User | undefined) => {
       this.user = user;
+      if (this.user) {
+        this.form.patchValue({
+          email: this.user.email
+        });
+      }
     });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Form submitted:', this.form.value);
+    }
   }
 }
